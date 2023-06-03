789243049706b0d28ad9d306242bd28f448a630d
LEE-S-J-HUB <78992963+LEE-S-J-HUB@users.noreply.github.com>
2023-06-03 09:57:34 +0900

    Add

--- provider.tf
+++ provider.tf
@@ -4,12 +4,10 @@ provider "aws" {
 }
 
 terraform {
-    backend "aws" {
-            backend "s3" {
-            bucket = "s3-an2-sha-dev-tf-jenkins" 
-            key    = "jenkins_temp/terraform.tfstate"
-            region = "ap-northeast-2"
-            encrypt = true
-        }
+    backend "s3" {
+        bucket = "s3-an2-sha-dev-tf-jenkins" 
+        key    = "jenkins_temp/terraform.tfstate"
+        region = "ap-northeast-2"
+        encrypt = true
     }
 }
